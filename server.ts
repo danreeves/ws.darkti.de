a10a776edb279e69932429cd8902137501d4910e
Dan Reeves <hey@danreev.es>
2023-12-18 23:57:14 +0000

    basic ws server

---
+++ server.ts
@@ -0,0 +1,30 @@
+Deno.serve({
+  port: 80,
+  handler: async (request) => {
+    // If the request is a websocket upgrade,
+    // we need to use the Deno.upgradeWebSocket helper
+    if (request.headers.get("upgrade") === "websocket") {
+      const { socket, response } = Deno.upgradeWebSocket(request);
+
+      socket.onopen = () => {
+        console.log("Client connected");
+      };
+      socket.onmessage = (event) => {
+        console.log(`RECEIVED: ${event.data}`);
+      };
+      socket.onclose = () => {
+        console.log("Client disconnected");
+      };
+      socket.onerror = (error) => {
+        console.error("ERROR:", error);
+      };
+
+      return response;
+    } else {
+      // If the request is a normal HTTP request,
+      // we serve the client HTML file.
+      const file = await Deno.open("./index.html", { read: true });
+      return new Response(file.readable);
+    }
+  },
+});
